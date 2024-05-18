2c4a25d21db024f26e22dc77cddff8472ba3fdb6
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-18 20:28:39 +0500

    Additions

--- app.js
+++ app.js
@@ -4,47 +4,32 @@ let underline = document.getElementById("underline");
 let text = document.getElementById("writtenText");
 
 
-// let fontSize = document.getElementsByClassName("fontSizeValue")
 
-// for( let i =0;i<fontSize.length;i++){
 
-//     fontSize[i].addEventListener('click',()=>{
-//        var font =   this.value;
-//        console.log(font)
-//     })
-// }
 
-function fontSizeHaiYe(num){
-    console.log(num)
-    text.style.fontSize = num+"px"
+function fontss() {
+  console.log(fontss.value);
 }
 
-function fontss(){
-    console.log(fontss)
-}
-
-
-
-// EvenListners 
+// EvenListners
 
 bold.addEventListener("click", () => {
-operating()
+  operating();
 
   text.style.fontWeight = "bold";
 });
 italic.addEventListener("click", () => {
-
-operating()
+  operating();
   text.style.fontStyle = "italic";
 });
 underline.addEventListener("click", () => {
-operating()
+  operating();
 
   text.style.textDecoration = "underline";
 });
 
-function operating(){
-    text.style.fontWeight = "400";
-    text.style.fontStyle = "normal";
-    text.style.textDecoration = "none";
-} 
+function operating() {
+  text.style.fontWeight = "400";
+  text.style.fontStyle = "normal";
+  text.style.textDecoration = "none";
+}
