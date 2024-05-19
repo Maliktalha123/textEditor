b13783756a0e94e1282ec978d7a03b8d7541c196
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-19 09:57:19 +0500

    Added Input Box and Stylling

--- app.js
+++ app.js
@@ -5,43 +5,84 @@ let bold = document.getElementById("bold");
 let italic = document.getElementById("italic");
 let underline = document.getElementById("underline");
 let text = document.getElementById("writtenText");
-
+let input = document.getElementById("input");
 // EvenListners
+function eee() {
+  console.log(input.value);
+  text.innerHTML = input.value
+}
 
-heading.addEventListener("click",()=>{
+
+heading.addEventListener("click", () => {
   operating();
-  text.style.fontSize = "36px"
-})
+  deactiveFunction();
+  heading.style.color = "blue";
+  heading.style.textDecoration = "underline";
 
-lowerCase.addEventListener("click",()=>{
-  operating()
- text = text.toLowerCase()
-})
+  text.style.fontSize = "36px";
+});
 
+capitialize.addEventListener("click", () => {
+  operating();
+  deactiveFunction();
+  capitialize.style.color = "blue";
+  capitialize.style.textDecoration = "underline";
+  text.style.textTransform = "uppercase";
+});
 
+lowerCase.addEventListener("click", () => {
+  operating();
+  deactiveFunction();
+  lowerCase.style.color = "blue";
+  lowerCase.style.textDecoration = "underline";
 
-
-
-
-
+  text.style.textTransform = "lowercase ";
+});
 
 bold.addEventListener("click", () => {
   operating();
+  deactiveFunction();
+  bold.style.color = "blue";
+  bold.style.textDecoration = "underline";
 
   text.style.fontWeight = "bold";
 });
 italic.addEventListener("click", () => {
   operating();
+  deactiveFunction();
+  italic.style.color = "blue";
+  italic.style.textDecoration = "underline";
+
   text.style.fontStyle = "italic";
 });
 underline.addEventListener("click", () => {
   operating();
+  deactiveFunction();
 
+  underline.style.color = "blue";
+  underline.style.textDecoration = "underline";
   text.style.textDecoration = "underline";
 });
 
 function operating() {
+  // .style.color = "black";
+  text.style.textDecoration = "none";
+  text.style.fontSize = "22px";
   text.style.fontWeight = "400";
   text.style.fontStyle = "normal";
   text.style.textDecoration = "none";
+}
+function deactiveFunction() {
+  heading.style.color = "black";
+  heading.style.textDecoration = "none";
+  lowerCase.style.color = "black";
+  lowerCase.style.textDecoration = "none";
+  capitialize.style.color = "black";
+  capitialize.style.textDecoration = "none";
+  bold.style.color = "black";
+  bold.style.textDecoration = "none";
+  italic.style.color = "black";
+  italic.style.textDecoration = "none";
+  underline.style.color = "black";
+  underline.style.textDecoration = "none";
 }
